a39a2eb8fc5663e1707236546b1b6cee78403c3b
dev7gy <fun7gy125@gmail.com>
2021-08-02 15:37:09 +0900

    Add HelloCoding_Algorithm Chapter1, Chapter2

---
+++ HelloCoding_Algorithm/C_Sharp_Algorithm/C_Sharp_Algorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Chapter1 ###");
+
+            Chapter1 chap1 = new Chapter1();
+            int[] sortedArray = { 1, 2, 3, 4, 5 };
+            int searchItem = 4;
+            Console.WriteLine($"index: {chap1.BasicSearch(sortedArray, searchItem)}");
+            Console.WriteLine($"index: {chap1.BinarySearch(sortedArray, searchItem)}");
+
+            Console.WriteLine("### Chapter1 ###");
+
+            Chapter2 chap2 = new Chapter2();
+            List<int> array = chap2.createRandomValues(7, 10);
+            foreach (var n in array)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+            List<int> sortedList = chap2.selectionSort(array);
+            foreach (var n in sortedList)
+            {
+                Console.Write($"{n} ");
+            }
+
+        }
+    }
+}
