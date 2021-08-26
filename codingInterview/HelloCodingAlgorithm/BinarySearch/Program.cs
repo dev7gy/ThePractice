d85cdbdbebd4433b55406846ccd25df62f4a180b
dev7gy <fun7gy125@gmail.com>
2021-08-26 20:37:57 +0900

    Add chapter1, chapter2 #HelloCodingAlgorithm

---
+++ codingInterview/HelloCodingAlgorithm/BinarySearch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sortedArray = { 1, 2, 3, 4, 5 };
+            int searchItem = 4;
+            Console.WriteLine($"index: {BasicSearch(sortedArray, searchItem)}");
+            Console.WriteLine($"index: {BinarySearch(sortedArray, searchItem)}");
+        }
+
+        /*
+         * log 에서 밑, 지수는 밑을 몇번 곱해야 지수가 될까? 라고 묻는것이다.
+         */
+        public static int BasicSearch(int[] sortedArray, int searchItem)
+        {
+            int index = -1;
+            int count = 0;
+            for (int i = 0; i < sortedArray.Length; ++i)
+            {
+                count++;
+                if (sortedArray[i] == searchItem)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Basic Search Count: {count}");
+            return index;
+        }
+
+        public static int BinarySearch(int[] sortedArray, int searchItem)
+        {
+            int low = 0;
+            int high = sortedArray.Length - 1;
+
+
+            int count = 0;
+            int index = 0;
+            while (count < sortedArray.Length) // while (low <= high)
+            {
+                count++;
+                int mid = (low + high) / 2;
+                if (sortedArray[mid] == searchItem)
+                {
+                    index = mid;
+                    break;
+                }
+                else if (sortedArray[mid] > searchItem)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+
+            }
+
+            Console.WriteLine($"Binary Search Count: {count}");
+            return index;
+        }
+    }
+}
---
+++ codingInterview/HelloCodingAlgorithm/SelectionSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Chapter2 ###");
+
+            List<int> array = CreateRandomValues(7, 10);
+            foreach (var n in array)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+            List<int> sortedList = SelectionSort(array);
+            foreach (var n in sortedList)
+            {
+                Console.Write($"{n} ");
+            }
+        }
+        public static List<int> CreateRandomValues(int length, int max)
+        {
+            Random random = new Random();
+            List<int> randomValues = new List<int>();
+            for (int i = 0; i < length; i++)
+            {
+                randomValues.Add(random.Next() % max);
+            }
+            return randomValues;
+        }
+        public static List<int> SelectionSort(List<int> array)
+        {
+            List<int> sortedArray = new List<int>();
+            int startLength = array.Count;
+            for (int i = 0; i < startLength; i++)
+            {
+                int minValue = array[0];
+                for (int j = 0; j < array.Count; j++)
+                {
+                    if (minValue > array[j])
+                    {
+                        minValue = array[j];
+                    }
+                }
+                sortedArray.Add(minValue);
+                array.Remove(minValue);
+            }
+            return sortedArray;
+        }
+    }
+}
