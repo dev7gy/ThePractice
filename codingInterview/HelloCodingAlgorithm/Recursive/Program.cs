91fd445bf21679fd057b45fa18492f466f7e64ac
dev7gy <fun7gy125@gmail.com>
2021-08-30 23:33:19 +0900

    Edit Recursive Part #HelloCoding add

--- codingInterview/HelloCodingAlgorithm/Recursive/Program.cs
+++ codingInterview/HelloCodingAlgorithm/Recursive/Program.cs
@@ -6,7 +6,30 @@ namespace Recursive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 5, 19, 8, 1 };
         }
+        
+        public static void countdown(int i)
+        {
+            if (i < 0)
+            {
+                return;
+            }
+            Console.WriteLine(i);
+            countdown(i - 1);
+        }
+
+        public static int factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+        
     }
 }
