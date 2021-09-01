4a7ac36719fff1fd58d3914eada1792b7f6855e9
dev7gy <fun7gy125@gmail.com>
2021-09-02 01:04:27 +0900

    Add HashTable #HelloCoding

---
+++ codingInterview/HelloCodingAlgorithm/HashTable/HashMap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class HashMap
+    {
+        public int[] CostTable { get; private set; }
+        public HashMap(int num)
+        {
+            CostTable = new int[num];
+            // int[] mCostTable = new int[num];
+        }
+
+        public void Add(string key, int value)
+        {
+            CostTable[HashFunction(key)] = value;
+        }
+        public int GetValue(string key)
+        {
+            long keyIndex = HashFunction(key);
+            if (keyIndex > -1 
+                && keyIndex < CostTable.Length 
+                && CostTable[keyIndex] > 0)
+            {
+                return CostTable[keyIndex];
+            }
+            return -1;
+        }
+
+        private long HashFunction(string str)
+        {
+            int tableLength = CostTable.Length;
+            long key = 0;
+            for (int i = 0; i < str.Length; ++i)
+            {
+                int temp = str[i];
+                key += temp;
+            }
+            return key % tableLength;
+        }
+    }
+}
---
+++ codingInterview/HelloCodingAlgorithm/HashTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashMap map = new HashMap(7);
+            string obj = "apple";
+            int cost = 100;
+            map.Add(obj, cost);
+            Console.WriteLine(map.GetValue(obj));
+            Console.WriteLine(map.GetValue("pear"));
+        }
+
+        
+
+
+    }
+}
