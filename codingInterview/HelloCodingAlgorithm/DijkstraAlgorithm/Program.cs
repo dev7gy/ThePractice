34e50de966005946b58c11aa370af6962eabe5be
dev7gy <fun7gy125@gmail.com>
2021-09-03 23:56:35 +0900

    Add DijkstraAlgorithm, Weighted Graphs #HelloCoding

---
+++ codingInterview/HelloCodingAlgorithm/DijkstraAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DijkstraAlgorithm
+{
+    /*
+     * 가중 그래프: 그래프의 간선에 가중치가 존재한다.
+     * < 다익스트라 알고리즘 >
+     * 1. 가장 가중치가 적게 드는 노드를 찾습니다.
+     * 2. 이 정점의 이웃 노드들의 가중치를 조사합니다.
+     * 3. 그래프 상의 모든 노드에 대해 이러한 일을 반복합니다.
+     * 4. 최종 경로를 계산합니다.
+     * 
+     * 다익스트라 알고리즘은 사이클을 지나면 최단 거리를 구할 수 없다.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
